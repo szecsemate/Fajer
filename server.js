5e7cb77b91796be87919dab50286e17f881d5cd5
szecsemate <kockagamer85@gmail.com>
2022-10-05 22:31:38 +0200

    made card dealing from backend

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 const { Console } = require("console");
 
-app.use(express.static("src"))
+
 
 const port = process.env.port || 3000;
 
@@ -14,22 +14,44 @@ const http = require("http").Server(app);
 
 const io = require("socket.io")(http);
 
+
 app.get("/", (req,res) =>
 {
     console.log("test")
+    res.sendFile(path.join(__dirname, "src/menu/menu.html"))
+});
+
+app.use(express.static(__dirname + "/src"));
+
+app.get("/Jatek", (req,res) =>
+{
     res.sendFile(path.join(__dirname, "src/index.html"))
 });
 
+var playercount = 0;
 
-io.on("connection" , socket =>
-{
-    console.log(socket.id)
 
-      socket.on("send-chat", (string)=>
-      {
-      io.emit("receive-chat", string);
-      })
-});
+    io.on("connection" , socket =>
+    {
+        if(playercount < 4)
+        {
+            playercount++
+            var egyenleg = 100
+            console.log(socket.id)
+            
+            socket.emit("cards", cards[playercount*3], cards[playercount*3+1], cards[playercount*3+2])
+    
+            socket.on("send-chat", (string)=>
+            {
+            io.emit("receive-chat", string);
+            })
+        }
+        else
+        {
+            socket.emit("full")
+        }
+        
+    });
 
 
 
@@ -38,3 +60,57 @@ http.listen(port, (req,res) =>
     console.log("http is listening on port " + port)
 });
 
+var cards=new Array(
+    "Tell-A-0A.png",
+    "Tell-A-07.png",
+    "Tell-A-08.png",
+    "Tell-A-09.png",
+    "Tell-A-10.png",
+    "Tell-A-J1.png",
+    "Tell-A-J2.png",
+    "Tell-A-KI.png",
+    "Tell-B-0A.png",
+    "Tell-B-0A.png",
+    "Tell-B-07.png",
+    "Tell-B-08.png",
+    "Tell-B-09.png",
+    "Tell-B-10.png",
+    "Tell-B-J1.png",
+    "Tell-B-J2.png",
+    "Tell-B-KI.png",
+    "Tell-H-0A.png",
+    "Tell-H-07.png",
+    "Tell-H-08.png",
+    "Tell-H-09.png",
+    "Tell-H-10.png",
+    "Tell-H-J1.png",
+    "Tell-H-J2.png",
+    "Tell-H-KI.png",
+    "Tell-L-0A.png",
+    "Tell-L-07.png",
+    "Tell-L-08.png",
+    "Tell-L-09.png",
+    "Tell-L-10.png",
+    "Tell-L-J1.png",
+    "Tell-L-J2.png",
+    "Tell-L-KI.png");
+    
+    cards = shuffle(cards);
+
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+          // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
--- src/socket_client.js
+++ src/socket_client.js
@@ -1,4 +1,13 @@
+var egyenleg = 100;
+var bent_van = 0;
+
+document.getElementById("balance").innerHTML = "Egyenleged: ".concat(egyenleg).concat("Ft");
+
 var socket = undefined;
+
+var Kartya1
+var Kartya2
+var Kartya3
 
 window.onload = function()
 {
@@ -14,8 +23,20 @@ window.onload = function()
         console.log(string)
         appendMessage(string)
     })
-}
 
+    socket.on("cards", (kartya1, kartya2, kartya3) => {
+        Kartya1 = kartya1
+        Kartya2 = kartya2
+        Kartya3 = kartya3
+    })
+
+    socket.on("full", () =>
+    {
+        alert("You fucked up")
+        window.location.replace("/menu/menu.html")
+    })
+
+}
 
 
 function appendMessage(message) {
@@ -28,6 +49,24 @@ function appendMessage(message) {
 
   function sendmessage()
   {
-
       socket.emit("send-chat", document.getElementById("uzenet").value)
   }
+
+
+  function emel()
+  {
+    var mennyivel = emeles_lekeres()
+    bent_van += mennyivel 
+
+    socket.emit("bet", mennyivel)
+
+    egyenleg -= mennyivel
+    document.getElementById("balance").innerHTML = "Egyenleged: ".concat(egyenleg).concat("Ft");
+  }
+
+  function megnez() {
+    document.getElementById("imgHand1").src = "cards/" + Kartya1;
+    document.getElementById("imgHand2").src = "cards/" + Kartya2;
+    document.getElementById("imgHand3").src = "cards/" + Kartya3;
+    document.getElementById("megnezem").blur();
+}
